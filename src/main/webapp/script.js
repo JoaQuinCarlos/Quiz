24cfed776409cdcb01ab648365f0b0982467adf5
Joppe <joakim.sather@hotmail.com>
2017-09-25 00:11:06 +0200

    Can now have multiple questions in a quiz

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,69 +1,67 @@
 /**
  * Created by Joppe on 19.09.2017.
  */
-
-var quizzes = 0;
-const theData = [];
-
-$("#submitQuestion").click(function(){
-   $.ajax({
-       url: 'rest/questions',
-       type: 'POST',
-       data: JSON.stringify({
-           svar: [$("#answer1").val(), $("#answer2").val(), $("#answer3").val(), $("#answer4").val()],
-           spmtext: $("#questionText").val(),
-           riktigIndex: $("#correctAnswer").val(),
-           tid: $("#duration").val()
-       }),
-       contentType: 'application/json; charset=utf-8',
-       datatype: 'json'
-   });
-});
-
-$("#makeQuiz").click(function(){
-    $.ajax({
-        url: 'rest/quiz',
-        type: 'POST',
-        data: JSON.stringify({
-            navn: $("#quizName").val(),
-            startTid: $("#startTime").val()
-        }),
-        success: function(){
-            quizzes ++;
-        },
-        contentType: 'application/json; charset=utf-8',
-        datatype: 'json'
-    });
-});
-
-$("#submitQuestion").click(function(){
-    $.ajax({
-        url: 'rest/quiz',
-        type: 'POST',
-        data: JSON.stringify({
-            navn: $("#quizName").val(),
-            startTid: $("#startTime").val(),
-            question:[{
-                svar: [$("#answer1").val(), $("#answer2").val(), $("#answer3").val(), $("#answer4").val()],
-                spmtext: $("#questionText").val(),
-                riktigIndex: $("#correctAnswer").val(),
-                tid: $("#duration").val()
-            }]
-        }),
-        contentType: 'application/json; charset=utf-8',
-        datatype: 'json'
-    });
-});
-
-
-
 $(document).ready(function(){
+
+const theData = [];
+var questions = [];
+
+    $("#submitQuestion").click(function(){
+        var ans1, ans2, ans3, ans4, spmtext, riktigIndex, tid;
+        ans1 = $("#answer1").val();
+        ans2 = $("#answer2").val();
+        ans3 = $("#answer3").val();
+        ans4 = $("#answer4").val();
+        spmtext = $("#questionText").val();
+        riktigIndex = $("#correctAnswer").val();
+        tid = $("#duration").val();
+
+        var question = {
+            svar: [ans1, ans2, ans3, ans4],
+            spmtext: spmtext,
+            riktigIndex: riktigIndex,
+            tid: tid
+        };
+        questions.push(question);
+
+        $("#answer1").val("");
+        $("#answer2").val("");
+        $("#answer3").val("");
+        $("#answer4").val("");
+        $("#questionText").val("");
+        $("#correctAnswer").val("");
+        $("#duration").val("");
+    });
+
+    $("#submitQuiz").click(function(){
+        var question = {
+            svar: [$("#answer1").val(), $("#answer2").val(), $("#answer3").val(), $("#answer4").val()],
+            spmtext: $("#questionText").val(),
+            riktigIndex: $("#correctAnswer").val(),
+            tid: $("#duration").val()
+        };
+        questions.push(question);
+        $.ajax({
+            url: 'rest/quiz',
+            type: 'POST',
+            data: JSON.stringify({
+                navn: $("#quizName").val(),
+                startTid: $("#startTime").val(),
+                question: questions
+            }),
+            contentType: 'application/json; charset=utf-8',
+            datatype: 'json'
+        });
+        questions = [];
+    });
+
     $.getJSON("rest/quiz", function(data){
         $.each(data, function(key, val){
             theData.push(val);
         });
         showQuizzes();
     });
+
     function showQuizzes(){
         $("#body").html("");
         for(var i = 0; i < theData.length; i++){
@@ -73,9 +71,9 @@ $(document).ready(function(){
                 '<td>' + theData[i].id + '</td>' +
                 '<td>' + theData[i].navn + '</td>' +
                 '<td>' + questions + '</td>' +
-                '<td>' + new Date() + '</td>' +
-                '<td><span class="glyphicon glyphicon-ok"></span></td>' +
-                '<td><span class="glyphicon glyphicon-stats"></span></td>' +
+                '<td>' + theData[i].startTid.toString() + '</td>' +
+                '<td><a href="quiz.html"><span class="glyphicon glyphicon-ok"></span></a></td>' +
+                '<td><a href="scores.html"><span class="glyphicon glyphicon-stats"></span></a></td>' +
                 '</tr>');
         }
     }
